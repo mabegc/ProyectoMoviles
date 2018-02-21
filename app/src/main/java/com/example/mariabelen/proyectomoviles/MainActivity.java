10975175eaf2b11f209e0b1e85f3410d1da9c54f
LAPMABE\Maria Belen <mabegc94@gmail.com>
2018-02-21 17:55:26 -0500

    khsz

---
+++ app/src/main/java/com/example/mariabelen/proyectomoviles/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mariabelen.proyectomoviles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
